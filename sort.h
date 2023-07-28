8704fa5a73b35f82fee2aef6b44e0d4e94e3b8ac
kazibweyassin <kazibweusama@gmail.com>
2023-07-28 05:31:22 -0700

    counting

--- sort.h
+++ sort.h
@@ -34,4 +34,5 @@ void quick_sort(int *array, size_t size);
 int partition(int *array, size_t lb, size_t rb);
 
 void merge_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 #endif
